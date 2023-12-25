d5367d75744435e21308c2c252f4c1a453ec6eb7
albert-abzalimov <alb.abzalimov@gmail.com>
2023-12-25 13:14:17 -0500

    Day 1: Messing around

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+const unsigned int width = 1080;
+const unsigned int height = 720;
+using namespace std;
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+int main() {
+    auto window = sf::RenderWindow{{width, height}, "CMake SFML Project"};
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        for (auto event = sf::Event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+            // Keyboard events
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                cout << "We clicked a button!" << endl;
             }
         }
 
-        window.clear();
+        window.clear(sf::Color(23, 23, 23));
+        // * Create a green circle
+        sf::CircleShape ball(50.f);
+        ball.setFillColor(sf::Color::Green);
+        ball.setPosition(width / 2.f, height / 2.f);
+        window.draw(ball);
         window.display();
     }
 }
